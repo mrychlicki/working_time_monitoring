e27f82f683e70acdac5fd45df4993b9e82207698
mrychlicki <mateusz.rychlicki@microsoft.wsei.edu.pl>
2020-03-03 11:53:12 +0100

    create solution

---
+++ working_time_monitoring_management/WTMM_Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace working_time_monitoring_management
+{
+    class WTMM_Class
+    {
+    }
+}
