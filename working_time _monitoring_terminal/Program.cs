b129662de858d3466fae7c99e174e5d4ac726305
mrychlicki <mateusz.rychlicki@microsoft.wsei.edu.pl>
2020-03-16 23:01:59 +0100

    add terminal options

--- working_time _monitoring_terminal/Program.cs
+++ working_time _monitoring_terminal/Program.cs
@@ -6,22 +6,33 @@ namespace working_time__monitoring_terminal
     {
         static void Main(string[] args)
         {
-            string end = "";
-            while (end != "exit")
-            {
-                string number = Console.ReadLine();
-                if(number == "exit")
-                {
-                    end = "exit";
-                }
-                else
-                {
-                    int card_number = int.Parse(number);
-                    Terminal_Add.add(card_number);
+              string end = "";
+              while (end != "exit")
+              {
+                  string number = Console.ReadLine();
+                  if(number == "exit")
+                  {
+                      end = "exit";
+                  }
+                  else
+                  {
+                      int card_number = int.Parse(number);
+                     bool check = Verification.check(card_number);
 
-                }
-            }
-            
+                     if (check == false)
+                     {
+                         Terminal_Add.add(card_number);
+
+                     }
+                     else
+                     {
+                         Update_end_time.add_end_time(card_number);
+                     }
+
+                 }
+              }
+         
+
         }
     }
 }
--- working_time _monitoring_terminal/WTMT.cs
+++ working_time _monitoring_terminal/WTMT.cs
@@ -5,6 +5,15 @@ using System.Text;
 
 namespace working_time__monitoring_terminal
 {
+    public class Date
+    {
+        public static string date()
+        {
+            DateTime dateAndTime = DateTime.Now;
+            string date = dateAndTime.ToString("yyyy-MM-dd");
+            return date;
+        }
+    }
     public class Terminal_Add
     {
         public static void add(int card_number)
@@ -15,6 +24,45 @@ namespace working_time__monitoring_terminal
             var cmd = new MySqlCommand();
             cmd.Connection = con;
             cmd.CommandText = $"INSERT INTO time_monitoring (FK_ID) SELECT e.ID FROM employee as e WHERE e.Card_number like {card_number}";
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+    
+    public class Verification
+    {
+        public static bool check(int card_number)
+        {
+            string cs = @"server=localhost;userid=Administrator;password=@dministrato;database=db_working_time";
+            var con = new MySqlConnection(cs);
+            con.Open();
+            var cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"SELECT COUNT(e.ID) FROM employee as e JOIN time_monitoring as tm on e.ID=tm.FK_ID WHERE e.Card_number LIKE {card_number} and tm.date LIKE '{Date.date()}'";
+            var rdr = cmd.ExecuteReader();
+            rdr.Read();
+            int x = rdr.GetInt32(0);
+            if (x == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+    public class Update_end_time
+    {
+        public static void add_end_time(int card_number)
+        {
+            string cs = @"server=localhost;userid=Administrator;password=@dministrato;database=db_working_time";
+            var con = new MySqlConnection(cs);
+            con.Open();
+            var cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"update time_monitoring as tm JOIN employee as e on tm.FK_ID=e.ID SET tm.time_end=CURRENT_TIMESTAMP WHERE e.Card_number like {card_number} and tm.date LIKE '{Date.date()}'";
             cmd.ExecuteNonQuery();
 
         }
