1a4915df90b8626ba8863d796c60b19df0215949
mrychlicki <mateusz.rychlicki@microsoft.wsei.edu.pl>
2020-03-16 11:41:23 +0100

    add terminal app

---
+++ working_time _monitoring_terminal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace working_time__monitoring_terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string end = "";
+            while (end != "exit")
+            {
+                string number = Console.ReadLine();
+                if(number == "exit")
+                {
+                    end = "exit";
+                }
+                else
+                {
+                    int card_number = int.Parse(number);
+                    Terminal_Add.add(card_number);
+
+                }
+            }
+            
+        }
+    }
+}
---
+++ working_time _monitoring_terminal/WTMT.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace working_time__monitoring_terminal
+{
+    public class Terminal_Add
+    {
+        public static void add(int card_number)
+        {
+            string cs = @"server=localhost;userid=Administrator;password=@dministrato;database=db_working_time";
+            var con = new MySqlConnection(cs);
+            con.Open();
+            var cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"INSERT INTO time_monitoring (FK_ID) SELECT e.ID FROM employee as e WHERE e.Card_number like {card_number}";
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
